44f2ee9521e0765c51aa06406dfcd2948f058bc7
aidannairn <aidan.nairn95@gmail.com>
2019-12-18 17:28:57 +1300

    User can input text into login form.

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +14,30 @@ const Register = () => {
   const onChange = event =>
     setFormData({ ...formData, [event.target.name]: event.target.value })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault()
     if (password !== password2) {
       console.log('Passwords do not match.')
     } else {
-      console.log(formData)
+      console.log('Success')
+      // REGISTER USER WITHOUT REDUX
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // }
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   }
+      //   const body = JSON.stringify(newUser)
+      //   const res = await axios.post('/api/users', body, config)
+      //   console.log(res.data)
+      // } catch (error) {
+      //   console.error(error.response.data)
+      // }
     }
   }
 
@@ -78,7 +97,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   )
