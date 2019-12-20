26210db3717937237a4ce99d1da537725c3c3117
aidannairn <aidan.nairn95@gmail.com>
2019-12-20 20:43:21 +1300

    Add NotFound route.

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="fas fa-code"></i> DevCommunity
         </Link>
       </h1>
       {!loading && (
