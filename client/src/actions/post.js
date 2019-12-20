650c0a875d4d500ac9fd53e035292486aacff443
aidannairn <aidan.nairn95@gmail.com>
2019-12-20 17:35:08 +1300

    User can delete posts that they have created.

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // Files
 import { setAlert } from './alert'
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types'
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types'
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -24,13 +24,13 @@ export const getPosts = () => async dispatch => {
 }
 
 // Add like
-export const addLike = postId => async dispatch => {
+export const addLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/like/${postId}`)
+    const res = await axios.put(`/api/posts/like/${id}`)
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { postId, likes: res.data }
+      payload: { id, likes: res.data }
     })
   } catch (error) {
     dispatch({
@@ -44,14 +44,36 @@ export const addLike = postId => async dispatch => {
 }
 
 // Remove like
-export const removeLike = postId => async dispatch => {
+export const removeLike = id => async dispatch => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${postId}`)
+    const res = await axios.put(`/api/posts/unlike/${id}`)
 
     dispatch({
       type: UPDATE_LIKES,
-      payload: { postId, likes: res.data }
+      payload: { id, likes: res.data }
     })
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status
+      }
+    })
+  }
+}
+
+// Delete post
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`)
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    })
+
+    dispatch(setAlert('Post Removed', 'success'))
   } catch (error) {
     dispatch({
       type: POST_ERROR,
