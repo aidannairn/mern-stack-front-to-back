dab74ba7de854ae5096afe5435f586b3bd1d59a2
aidannairn <aidan.nairn95@gmail.com>
2019-12-20 19:12:41 +1300

    Add comments to database.

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import { getPost } from '../../actions/post'
 import PostItem from '../posts/PostItem'
+import CommentForm from '../post/CommentForm'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -20,6 +21,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   )
 }
